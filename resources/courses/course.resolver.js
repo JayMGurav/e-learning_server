94daa840aab72edcf7386d1dd00759539dd36c58
Mrithyunjay Gurav <mrithyunjay2799@gmail.com>
2020-05-04 20:04:03 +0530

    added resolver to courses

--- resources/courses/course.resolver.js
+++ resources/courses/course.resolver.js
@@ -1,17 +1,10 @@
-// module.exports = {
-// boughtBy: async (course, args, { models, user }) =>
-//     await models.User.findById(course.instructor)
-//         .sort({ _id: -1 })
-//         .exec()
-// };
+var mongoose = require('mongoose');
 module.exports = {
-    // instructor: async (course, args, { models }) => {
-    //     return await models.Instructor.findById(course.instructor)
-    //         .sort({ _id: -1 })
-    //         .exec();
-    // }
-    // boughtBy: async (course, args, { models, user }) =>
-    // await models.User.find({_id:{$in:}})
-    //     .sort({ _id: -1 })
-    //     .exec()
+    instructor: async (course, args, { models }) => {
+        return await models.Instructor.findOne({
+            courses: mongoose.Types.ObjectId(course._id)
+        })
+            .sort({ _id: -1 })
+            .exec();
+    }
 };
